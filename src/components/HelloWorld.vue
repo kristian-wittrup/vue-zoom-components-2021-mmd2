747e7af34d97c5b4400d27fed44c901b7fcb24e3
kristian-wittrup <kw@easv.dk>
2021-03-08 11:47:51 +0100

    toggle stuff, almost working

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,93 +1,46 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
+    <!--
+    <div>
+      <p>{{ hello }}</p>
+      <button @click="changeContent()">
+        Click me
+      </button>
+    </div>
+    -->
+    <!-- Toggle basic : This will toggle all with same v-if 
+    <div v-if="showContent">
+      <p> {{ title }} </p>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
+    </div>
+    <div v-if="showContent">
+      <p> {{ titleTwo }} </p>
 
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
+    </div>
+    -->
+    <!--
+    <button @click="toogleContent">
+      Show/hide content
+    </button>-->
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
+    
 
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
+<!-- Toggle Individual item start -->
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
+    <div v-for="item in memesLib" :key="item.index">
+      <div v-show="item.toogleInd">
+        <p>{{ item.memeTitle }}</p>
+        <img :src="item.imgStuff" alt="" height=200>
+        <button @click="toggleInd(item)">Click</button>
+      </div>
+      <div v-if="!item.toogleInd">
+        MNeeee
+      </div>
+    </div>
 
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
 
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
+
   </v-container>
 </template>
 
@@ -96,56 +49,39 @@
     name: 'HelloWorld',
 
     data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
+/* 
+      hello: "Say hello to Homer",
+      title : "Show me da money",
+      titleTwo : "Im freeee",
+      showContent: true
+*/      
+    
+    memesLib: [
+      {
+        memeTitle : "Show me the cat",
+        imgStuff: require('../assets/vibe.gif'),
+        toogleInd: false
+      },
+      {
+        memeTitle : "Show me the dog",
+        imgStuff: require('../assets/dogcar.gif'),
+        toogleInd: true
+      },
+      
+    ]
+
     }),
+    methods: {
+      toggleInd(item) {
+        item.toggleInd = !item.toggleInd
+     
+      },
+      toogleContent() {
+        this.showContent = !this.showContent
+      },
+      changeContent() {
+        this.hello = "Say hello to Grandpa"
+      }
+    }
   }
 </script>
